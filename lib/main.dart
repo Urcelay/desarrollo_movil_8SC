fb1d341b02612f3cef59fb9a501b09cac01a494c
Urcelay <fernandaurcelay5@gmail.com>
2025-08-28 16:48:46 -0600

    feat (rive): animation added

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart'; //libreria material
+import 'screens/login_screen.dart'; //importa la pantalla de login
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget { //sin estado 
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
+
